39f217ca2cffeae54bfffbbb17ed27bc6afd8925
Active0000 <55964561+Active0000@users.noreply.github.com>
2020-10-19 16:48:11 -0700

    Added files via PC
    The new main.js as of today.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const ms = require("ms");
+
 // const ytdl = require("ytdl-core");
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
@@ -20,16 +20,64 @@ client.once('ready', () => {
 
 client.on('message', async (message) => {
 
-    const textP = message.content;
+    const textP = message.content.toLowerCase();
     switch (textP) {
         case 'bro': 
             message.channel.send('sup bro');
             break;
-        case 'bro': 
-            message.channel.send('bruh');
+        case 'good bot': 
+            message.channel.send('thank you uwu');
+            break;
+        case 'smart bot': 
+            message.channel.send('I HAVE A BIG BRAIN!');
+            break;
+        case 'bad bot': 
+            message.channel.send(':c');
+            break;
+        case 'sexy bot': 
+            message.channel.send('owo');
+            break;
+        case 'dumb bot': 
+            message.channel.send('durrrrrrrrrrrr');
+            break;
+        case 'destiny': 
+            message.channel.send('des-TINY the gnome manlet');
+            break;
+        case 'based': 
+            message.channel.send('Based on what?');
+            break;
+        case 'fucking based': 
+            message.channel.send('Hello, is this the based department??');
+            break;
+        case 'sunshine': 
+            message.channel.send('Sunshineeee <3');
+            break;
+        case 'weirdo': 
+            message.channel.send('im not a weirdo QQ');
+            break;
+        case 'anyone tryna run': 
+            message.channel.send('yeah...but not with you...');
+            break;
+        case 'black people': 
+            message.channel.send('do u luh blah ppl?');
+            break;
+        case 'white people': 
+            message.channel.send('white ppl smh');
+            break;
+        case 'pog': 
+            message.channel.send('poggerssss');
+            break;
+        case 'tru': 
+            message.channel.send('FUCKINGGGGGGGGGG  TRUUUUUUUUUUUUUUUUUUUUUUUUUUUU');
+            break;
+        case 'bruh': 
+            message.channel.send('b-bruhhh');
             break;
         case '-sigh-': 
             message.channel.send('i wish i could sigh');
+            break;
+        case 'hmph': 
+            message.channel.send('ooooo...they mad o.O');
             break;
         case 'T-T': 
             message.channel.send('Cry some more');
@@ -62,6 +110,8 @@ client.on('message', async (message) => {
         client.commands.get('tomorrow').execute(message, args);
     } else if (command == 'yesterday') {
         client.commands.get('yesterday').execute(message, args);
+    }else if (command == 'list') {
+        client.commands.get('list').execute(message, args);
     } else if (command == 'timer1min') {
         message.channel.send("Timer set for 1 minute!");
         setTimeout(() => { message.channel.send("It's been 1 minute! @everyone"); }, 10000);
@@ -74,148 +124,26 @@ client.on('message', async (message) => {
     } else if (command == 'timer1hour') {
         message.channel.send("Timer set for 1 hour!");
         setTimeout(() => { message.channel.send("It's been 1 hour! @everyone"); }, 3600000);
+
+
+    
     } else if (command == 'kick') {
-        const modRole = message.guild.roles.cache.find(role => role.name === "Mods");
-        if (!modRole)
-            return console.log("The Mods role does not exist");
-
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply("You can't use this command.");
-
-        if (!args[0]) message.channel.send('You need to @ a member!');
-
-        const user = message.mentions.users.first();
-
-        if (user) {
-            const member = message.guild.member(user);
-
-            if (member) {
-                member.kick("Member has been kicked.").then(() => {
-                    message.reply(`Sucessfully kicked ${user.tag}`);
-                }).catch(err => {
-                    message.reply('Unable to kick member.');
-                    console.log(err);
-                });
-            } else {
-                message.reply("That user is not in the server.");
-            }
-        } else {
-            message.reply("That user is not in the guild.");
-        }
+        client.commands.get('kick').execute(message, args);
 
     } else if (command == 'ban') {
-        const modRole = message.guild.roles.cache.find(role => role.name === "Mods");
-        if (!modRole)
-            return console.log("The Mods role does not exist");
+        client.commands.get('ban').execute(message, args);
 
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply("You can't use this command.");
-
-        if (!args[0]) message.channel.send('You need to @ a member!');
-
-        const user = message.mentions.users.first();
-
-        if (user) {
-            const member = message.guild.member(user);
-
-            if (member) {
-                member.kick("Member has been BANNED.").then(() => {
-                    message.reply(`Sucessfully BANNED ${user.tag}`);
-                }).catch(err => {
-                    message.reply('Unable to ban member.');
-                    console.log(err);
-                });
-            } else {
-                message.reply("That user is not in the server.");
-            }
-        } else {
-            message.reply("That user is not in the guild.");
-        }
-
-    } else if (command == 'muteT') {
-        const modRole = message.guild.roles.cache.find(role => role.name === "Mods");
-        if (!modRole)
-            return console.log("The Mods role does not exist");
-
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply("You can't use this command.");
-
-
-        var person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-        if (!person) 
-            return message.reply("Couldn't find that member!");
-
-        let mainRole = message.guild.roles.cache.find(role => role.name === "User")
-        let muteRole = message.guild.roles.cache.find(role => role.name === "MuteUser")
-
-        if (!muteRole) 
-            return message.reply("Couldn't find that mute role!")
-
-        var time = args[1];
-
-        if (!time) {
-            return message.reply("You need to specify a time!");
-        }
-        person.roles.remove(mainRole.id);
-        person.roles.add(muteRole.id);
-
-        message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`);
-
-        setTimeout(function () {
-            person.roles.add(mainRole.id);
-            person.roles.remove(muteRole.id);
-
-            message.channel.send(`@${person.user.tag} has been timely unmuted!`)
-
-        }, ms(time));
+    } else if (command == 'timedmute') {
+        client.commands.get('timedmute').execute(message, args);
 
     } else if (command == 'unmute') {
-        const modRole = message.guild.roles.cache.find(role => role.name === "Mods");
-        if (!modRole)
-            return console.log("The Mods role does not exist");
-
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply("You can't use this command.");
-
-        var person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-        if (!person) 
-            return message.reply("Couldn't find that member!");
-
-        let mainRole = message.guild.roles.cache.find(role => role.name === "User")
-        let muteRole = message.guild.roles.cache.find(role => role.name === "MuteUser")
-
-        if (!muteRole) 
-            return message.reply("Couldn't find that mute role!")
-
-        person.roles.add(mainRole.id);
-        person.roles.remove(muteRole.id);
-
-        message.channel.send(`@${person.user.tag} has been manually unmuted!`)
+        client.commands.get('unmute').execute(message, args);
 
     } else if (command == 'mute') {
+        client.commands.get('mute').execute(message, args);
 
-        const modRole = message.guild.roles.cache.find(role => role.name === "Mods");
-        if (!modRole)
-            return console.log("The Mods role does not exist");
-
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply("You can't use this command.");
-
-
-        var person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-        if (!person) 
-            return message.reply("Couldn't find that member!");
-
-        let mainRole = message.guild.roles.cache.find(role => role.name === "User")
-        let muteRole = message.guild.roles.cache.find(role => role.name === "MuteUser")
-
-        if (!muteRole) 
-            return message.reply("Couldn't find that mute role!")
-
-        person.roles.add(muteRole.id);
-        person.roles.remove(mainRole.id);
-
-        message.channel.send(`@${person.user.tag} has been permanently muted!`)
+    } else if (command == 'prune') {
+        client.commands.get('prune').execute(message, args);
     }
 });
 client.login(token);
